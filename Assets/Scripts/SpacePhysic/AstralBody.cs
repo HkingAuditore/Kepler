ac87f93ef666095a6008cde41e3d9812ee898c82
HkingAuditore <178854663@qq.com>
2020-10-07 22:43:58 +0800

    基于物理的轨道

---
+++ Assets/Scripts/SpacePhysic/AstralBody.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using SpacePhysic;
+using UnityEngine;
+
+public class AstralBody : MonoBehaviour
+{
+    public float Mass { get; private set; }
+    public Vector3 oriVelocity;
+    public float radius;
+    private Rigidbody _rigidbody;
+    private List<AstralBody> _affectedPlanets = new List<AstralBody>();
+    private LineRenderer _lineRenderer;    
+    
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _rigidbody = GetComponent<Rigidbody>();
+        this.GetComponents<SphereCollider>()[1].radius = radius;
+        Mass = _rigidbody.mass;
+
+        _rigidbody.velocity = oriVelocity;
+
+    }
+
+    private void FixedUpdate()
+    {
+        this._rigidbody.AddForce(CalculateForce());
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       var astral =  other.GetComponent<AstralBody>();
+       if (astral != null)
+       {
+           astral._affectedPlanets.Add(this);
+       }
+    }
+
+    #region 牛顿万有引力
+
+    //计算目标到本星球的引力
+    private float CalculateGravityModulus(float targetMass, float distance) =>
+        PhysicBase.GetG() * (Mass * targetMass / (distance * distance));
+
+    public Vector3 GetGravityVector3(Rigidbody rigidbody)
+    {
+        float distance = Vector3.Distance(this.transform.position, rigidbody.position);
+        Vector3 normalizedDirection = (this.transform.position - rigidbody.position).normalized;
+        return CalculateGravityModulus(rigidbody.mass, distance) * normalizedDirection;
+    }
+        
+    //计算线速度向量
+    public Vector3 CalculateForce()
+    {
+        //List<Vector3> gravities = new List<Vector3>();
+        Vector3 forceResult = new Vector3(0,0,0);
+            
+        //计算引力向量集
+        foreach (AstralBody astralBody in _affectedPlanets)
+        {
+            //float distance = Vector3.Distance(this.transform.position, astralBody.gameObject.transform.position);
+            forceResult+=astralBody.GetGravityVector3(this._rigidbody);
+            //gravities.Add(astralBody.GetGravityVector3(this._rigidbody));
+        }
+
+        //计算合力
+        //Vector3 forceResult = gravities.Aggregate((r, v) => r + v);
+
+        return forceResult;
+    }
+        
+
+    #endregion
+}
